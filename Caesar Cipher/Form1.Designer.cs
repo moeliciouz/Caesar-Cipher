c927b6fde9ea386a15e2cd811abd8a07ad7a2cad
moeliciouz <moritz.b.hoffmann@gmail.com>
2017-05-05 16:25:01 +0200

    Caesar Cipher in C#
    This is the Caesar Cipher implemented in C# as a school project.

---
+++ Caesar Cipher/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Caesar_Cipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(132, 128);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(150, 13);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(132, 128);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(124, 161);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            26,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            26,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(47, 20);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Verschlüsseln";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(177, 203);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Entschlüsseln";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Caesar Cipher";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Caesar Cipher/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caesar_Cipher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string klarText = richTextBox1.Text;
+            string Verschlüsselung = "";
+            for (int i = 0; i < klarText.Length; i++)
+            {
+                klarText = klarText.ToLower();
+
+                if (klarText[i] + numericUpDown1.Value >= 123)
+                    Verschlüsselung += (char)((96 + (klarText[i] + numericUpDown1.Value)) % 122);
+
+                else if (klarText[i] + numericUpDown1.Value <= 96)
+                    Verschlüsselung += (char)(((klarText[i] + numericUpDown1.Value) + 26) % 123);
+
+                else Verschlüsselung += (char)((klarText[i] + numericUpDown1.Value) % 122);
+
+                richTextBox2.Text = Verschlüsselung;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string Cipher = richTextBox2.Text;
+            string Entschlüsselung = "";
+            for (int i = 0; i < Cipher.Length; i++)
+            {
+                Cipher = Cipher.ToLower();
+
+                if (Cipher[i] - numericUpDown1.Value >= 123)
+                    Entschlüsselung += (char)((96 + (Cipher[i] - numericUpDown1.Value)) % 122);
+
+                else if (Cipher[i] - numericUpDown1.Value <= 96)
+                    Entschlüsselung += (char)(((Cipher[i] - numericUpDown1.Value) + 26) % 122);
+
+                else Entschlüsselung += (char)((Cipher[i] - numericUpDown1.Value) % 122);
+
+                richTextBox1.Text = Entschlüsselung;
+            }
+        }
+    }
+}
